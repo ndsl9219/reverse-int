15b77c2838c82be3588ee05651cf08f9cc69ce22
ndsl9219 <ndsl971@proton.me>
2023-01-20 23:30:32 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let toModule = Math.abs(n);
+  let toString = String(toModule);
+  let result = toString.split('').reverse().join('');
+  return result;
 }
